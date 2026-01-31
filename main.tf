905d4acb6f28351f6a5f6c76bc90c7a2cd50b7a6
hiren <hiren1994>
2026-01-31 09:49:31 +0530

    upload

--- main.tf
+++ main.tf
@@ -1,85 +1,85 @@
 
-# Existing VPC (Data Source)
-# -----------------------------
-data "aws_vpc" "existing_vpc" {
-  id = var.vpc_id
-}
+# # Existing VPC (Data Source)
+# # -----------------------------
+# data "aws_vpc" "existing_vpc" {
+#   id = var.vpc_id
+# }
 
-# Existing Subnet (Data Source)
-# -----------------------------
-data "aws_subnet" "existing_subnet" {
-  id = var.subnet_id
-}
-# Existing Key Pair (Data Source)
-# -----------------------------
+# # Existing Subnet (Data Source)
+# # -----------------------------
+# data "aws_subnet" "existing_subnet" {
+#   id = var.subnet_id
+# }
+# # Existing Key Pair (Data Source)
+# # -----------------------------
 
-data "aws_key_pair" "existing" {
-  key_name = var.key_pair_name
-}
-
-
-# Ubuntu 22.04 AMI
-# -----------------------------
-data "aws_ami" "ubuntu_2204" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
+# data "aws_key_pair" "existing" {
+#   key_name = var.key_pair_name
+# }
 
 
-# Security Group (SSH only)
-# -----------------------------
-resource "aws_security_group" "ssh_sg" {
-  name        = "ec2-ssh-sg"
-  description = "Allow SSH access"
-  vpc_id      = data.aws_vpc.existing_vpc.id
+# # Ubuntu 22.04 AMI
+# # -----------------------------
+# data "aws_ami" "ubuntu_2204" {
+#   most_recent = true
+#   owners      = ["099720109477"] # Canonical
 
-  ingress {
-    description = "SSH access"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+# }
 
-  tags = {
-    Name = "ec2-ssh-sg"
-  }
-}
 
-# -----------------------------
-# EC2 Instance
-# -----------------------------
-resource "aws_instance" "ubuntu_ec2" {
-  depends_on             = [aws_security_group.ssh_sg]
-  ami                    = data.aws_ami.ubuntu_2204.id
-  instance_type          = var.instance_type
-  subnet_id              = data.aws_subnet.existing_subnet.id
-  vpc_security_group_ids = [aws_security_group.ssh_sg.id]
-  key_name               = data.aws_key_pair.existing.key_name
+# # Security Group (SSH only)
+# # -----------------------------
+# resource "aws_security_group" "ssh_sg" {
+#   name        = "ec2-ssh-sg"
+#   description = "Allow SSH access"
+#   vpc_id      = data.aws_vpc.existing_vpc.id
 
-  root_block_device {
-    volume_size = var.volume_size
-    volume_type = "gp3"
-  }
+#   ingress {
+#     description = "SSH access"
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = "ubuntu-22-04"
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   tags = {
+#     Name = "ec2-ssh-sg"
+#   }
+# }
+
+# # -----------------------------
+# # EC2 Instance
+# # -----------------------------
+# resource "aws_instance" "ubuntu_ec2" {
+#   depends_on             = [aws_security_group.ssh_sg]
+#   ami                    = data.aws_ami.ubuntu_2204.id
+#   instance_type          = var.instance_type
+#   subnet_id              = data.aws_subnet.existing_subnet.id
+#   vpc_security_group_ids = [aws_security_group.ssh_sg.id]
+#   key_name               = data.aws_key_pair.existing.key_name
+
+#   root_block_device {
+#     volume_size = var.volume_size
+#     volume_type = "gp3"
+#   }
+
+#   tags = {
+#     Name = "ubuntu-22-04"
+#   }
+# }
--- variables.tf
+++ variables.tf
@@ -1,35 +1,35 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
-}
+# variable "aws_region" {
+#   description = "AWS region"
+#   type        = string
+#   default     = "us-east-1"
+# }
 
-variable "vpc_id" {
-  description = "Existing VPC ID"
-  type        = string
-  default     = "vpc-08bb0cb0bdc397181"
-}
+# variable "vpc_id" {
+#   description = "Existing VPC ID"
+#   type        = string
+#   default     = "vpc-08bb0cb0bdc397181"
+# }
 
-variable "subnet_id" {
-  description = "Existing Subnet ID"
-  type        = string
-  default     = "subnet-033b3927bc0c2d24c"
-}
+# variable "subnet_id" {
+#   description = "Existing Subnet ID"
+#   type        = string
+#   default     = "subnet-033b3927bc0c2d24c"
+# }
 
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t3.micro"
-}
+# variable "instance_type" {
+#   description = "EC2 instance type"
+#   type        = string
+#   default     = "t3.micro"
+# }
 
-variable "key_pair_name" {
-  description = "Existing AWS key pair name"
-  type        = string
-  default     = "ubuntu-key"
-}
-variable "volume_size" {
-  description = "Size of the root EBS volume in GB"
-  type        = number
-  default     = 30
+# variable "key_pair_name" {
+#   description = "Existing AWS key pair name"
+#   type        = string
+#   default     = "ubuntu-key"
+# }
+# variable "volume_size" {
+#   description = "Size of the root EBS volume in GB"
+#   type        = number
+#   default     = 30
 
-}
+# }
